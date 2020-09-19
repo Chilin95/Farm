3409d97b2e121761d0770a2838189cde930a5c08
Chilin_Air <chilin.cn.ren@gmail.com>
2020-09-19 20:01:40 +0800

    add command features

--- index.js
+++ index.js
@@ -1,8 +1,116 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const config = require('./config.json');
+const prefix = config.prefix;
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+//Promise功能用起来，录音+Help
 
-client.login('your-token-goes-here');
+client.on('message', async message => {
+
+	const voiceChannel = message.member.voice.channel;
+	// Join the same voice channel of the author of the message
+	if (voiceChannel && message.content === '!join') {
+		const connection = await voiceChannel.join();
+		const audio = connection.receiver.createStream(message.author, { mode: 'pcm' });
+		audio.pipe(fs.createWriteStream('user_audio'));
+		
+	}
+
+	if (message.content === '!over') {
+		console.log('Agumon just leaved the vioce channel!');
+		try {
+			voiceChannel.leave();
+		}
+		catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to leave that channel!');
+		}
+	}
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (commandName === 'prune') {
+		const amount = parseInt(args[0]) + 1;
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		}
+		else if (amount <= 1 || amount > 100) {
+			return message.reply('you need to input a number between 1 and 99.');
+		}
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('there was an error trying to prune messages in this channel!');
+		});
+	}
+
+	
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
+});
+
+client.login(config.token);
